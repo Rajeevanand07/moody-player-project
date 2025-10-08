b96eef010b668bd0b5d3a089a2df2cf3aaedd40d
Rajeevanand07 <anandrajeev1234@gmail.com>
2025-10-09 02:00:37 +0530

    added other apis

--- backend/src/routes/song.route.js
+++ backend/src/routes/song.route.js
@@ -3,21 +3,40 @@ const express = require("express");
 const router = express.Router();
 const multer = require("multer");
 const uploadFile = require("../service/storage.service");
+const songModel = require("../models/song.model");
 
 const upload = multer({ storage: multer.memoryStorage() });
 
 router.post("/songs", upload.single("audio"), async (req, res) => {
   const fileData = await uploadFile(req.file);
-  console.log(fileData);
-
+  const song = await songModel.create({
+    title: req.body.title,
+    artist: req.body.artist,
+    audio: fileData.url,
+    mood: req.body.mood,
+  });
   res.status(201).json({
     message: "song created successfullly",
+    song: song,
   });
 });
 
-router.get("/songs", (req, res) => {
-  res.json({
+router.get("/songs", async (req, res) => {
+  const { mood } = req.query;
+  const songs = await songModel.find({
+    mood: mood,
+  });
+  res.status(200).json({
+    message: "song feched..",
+    songs: songs,
+  });
+});
+
+router.get("/", async (req, res) => {
+  const songs = await songModel.find({});
+  res.status(200).json({
     message: "songs",
+    songs: songs,
   });
 });
 
